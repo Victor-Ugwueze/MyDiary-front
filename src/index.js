8eb2fa580cc34d00bf95cf1ecfdd95a17906c3b9
victor <victorugwueze@gmail.com>
2018-11-18 23:27:35 +0100

    chore(setup react): setup up react project with react
    [Finishes #161938185]

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// react libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// compoents
+import App from './components/App';
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
